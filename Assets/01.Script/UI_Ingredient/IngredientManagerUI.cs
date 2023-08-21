b65fb1c3502ca5f26a75b851639a07d2bc5f587a
JINNY <sousaca05@gmail.com>
2023-08-21 15:35:00 +0900

    230821
    오개완

--- Assets/01.Script/UI_Ingredient/IngredientManagerUI.cs
+++ Assets/01.Script/UI_Ingredient/IngredientManagerUI.cs
@@ -1,5 +1,6 @@
 using JinnyBuilding;
 using JinnyProcessItem;
+using JinnyCropItem;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,21 +11,29 @@ using UnityEngine.UI;
 public class IngredientManagerUI : MonoBehaviour
 {
 
-    public Image image;            //움직일 상점 창 이미지 
-    public Vector3 endPosition;    //마지막 이동 위치
-    public float speed = 120f;     //이동 속도
+    //public Image image;            //움직일 상점 창 이미지 
+    //public Vector3 endPosition;    //마지막 이동 위치
+    //public float speed = 120f;     //이동 속도
 
     public GameObject copyBuilding;                   //건물 복제본
     public Vector3 uiOffset = new Vector3(-1, 1, 0);  //UI 위치 오프셋. 건물의 좌측 상단 모서리에 나타나게 만드는데 사용
 
+
+    //public RectTransform ret;
+    public GameObject arrow;  // Arrow 게임 오브젝트에 대한 참조
+    public RectTransform arrowRectTransform; // Arrow 오브젝트의 RectTransform 참조
+    public Image arrowImage;
+
+
+
     public static IngredientManagerUI Instance; //싱글톤
     //public IngredientSlot[] ingredientSlots;   //원재료 슬롯
     public Recipe specificRecipe;              //레시피
-    public IngredientSlot ingredientSlotPrefab;
+    public IngredientSlot ingredientSlotPrefab; // 원재료 프리팹
 
     public List<IngredientSlot> ingredientSlots = new List<IngredientSlot>(); //원재료 이미지 할당 슬롯
     public List<Image> productImageDisplays = new List<Image>();    //UI에 표시될 완성품 이미지
-    public Image imagePrefab;                                      //이미지 프리팹
+    public Image imagePrefab;                                      //완성품 프리팹
 
     private void Awake()
     {
@@ -50,6 +59,7 @@ public class IngredientManagerUI : MonoBehaviour
             //Debug.Log("건물 클릭됨?");
             Debug.Log("건물 클릭됨: " + hit.collider.gameObject.name);
             clickedBuilding = hit.collider.gameObject;
+         
         }
 
         //클릭된 건물이 없으면 반환
@@ -57,6 +67,26 @@ public class IngredientManagerUI : MonoBehaviour
 
         //클릭된 건물 copyBuilding에 저장
         copyBuilding = clickedBuilding;
+
+
+
+        // 복제본에서 arrowImage 참조를 설정
+        Transform arrowTransform = copyBuilding.transform.Find("finish image/arrow");
+        if (arrowTransform != null)
+        {
+            arrowImage = arrowTransform.GetComponent<Image>();
+            if (arrowImage == null)
+            {
+                Debug.LogError("The 'arrow' GameObject in the copy does not have an Image component.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Cannot find 'finish image/arrow' GameObject in the copy.");
+        }
+
+
+
 
         //UI 창 이동
         SetUIPosition(clickedBuilding);
@@ -91,7 +121,6 @@ public class IngredientManagerUI : MonoBehaviour
                 Debug.LogError("빌딩타입 " + buildingType + " is not present in buildingRecipes dictionary.");
             }
         }
-
     }
 
     public void ShowfinishIngredient(Recipe recipe)
@@ -117,7 +146,7 @@ public class IngredientManagerUI : MonoBehaviour
     {
         Debug.Log("원재료 함수쓰");
 
-        CreateIngredientSlotsForRecipe(recipe);
+        CreateIngredientSlot(recipe);
 
         for (int i = 0; i < ingredientSlots.Count; i++)
         {
@@ -202,30 +231,101 @@ public class IngredientManagerUI : MonoBehaviour
         }
     }
 
-    public void CreateIngredientSlotsForRecipe(Recipe recipe)
+    //원재료 슬롯 생성
+
+
+    public void CreateIngredientSlot(Recipe recipe)
     {
-        // 현재 슬롯 모두 비활성화
+        Debug.Log("원재료 슬롯 활성화?");
+
+        //현재 슬롯 모두 비활성화
         foreach (var slot in ingredientSlots)
         {
+            Debug.Log("비활성화");
             Destroy(slot.gameObject);
         }
         ingredientSlots.Clear();
 
+        
+
+
         // 레시피의 원재료 수에 맞게 슬롯 생성
         foreach (var ingredientObj in recipe.ingredients)
         {
-            IngredientSlot newSlot = Instantiate(ingredientSlotPrefab, transform);
+            Debug.Log("원재료 슬롯 생성 가능?");
+            IngredientSlot newSlot = Instantiate(ingredientSlotPrefab, arrow.transform);
+            
 
+
+            // 새로운 슬롯의 RectTransform을 가져옵니다
+            RectTransform newSlotRectTransform = newSlot.GetComponent<RectTransform>();
+
+            // 새로운 슬롯의 위치를 arrow의 아래로 설정합니다
+            //newSlotRectTransform.anchoredPosition = arrowRectTransform.anchoredPosition; // - new Vector2(0, (newSlotRectTransform.sizeDelta.y + arrowRectTransform.sizeDelta.y) * (ingredientSlots.Count + 1));
+
+            // 새로운 슬롯의 위치를 finish image 아래로 설정
+            newSlotRectTransform.anchoredPosition = arrowImage.rectTransform.anchoredPosition - new Vector2(0, (newSlotRectTransform.sizeDelta.y + arrowImage.rectTransform.sizeDelta.y) * (ingredientSlots.Count + 1));
+
+            Debug.Log("원재료 슬롯 위치" + arrowRectTransform.anchoredPosition);
+
+            // 원재료가 IItem 타입
             if (ingredientObj is Ingredient<IItem> itemIngredient)
             {
+                Debug.Log("if문");
                 newSlot.SetIngredient(itemIngredient.item, itemIngredient.quantity);
             }
+
+            // 원재료가 ProcessItemDataInfo 타입
             else if (ingredientObj is Ingredient<ProcessItemDataInfo> processedIngredient)
             {
+                Debug.Log("else if문");
                 newSlot.SetIngredient(new ProcessItemIItem(processedIngredient.item), processedIngredient.quantity);
             }
             ingredientSlots.Add(newSlot);
+
         }
     }
+
+
+
+
+
+    //public void CreateIngredientSlot(Recipe recipe)
+    //{
+    //    Debug.Log("원재료 슬롯 활성화?");
+
+    //    //현재 슬롯 모두 비활성화
+    //    foreach (var slot in ingredientSlots)
+    //    {
+    //        Debug.Log("비활성화");
+    //        Destroy(slot.gameObject);
+    //    }
+    //    ingredientSlots.Clear();
+
+    //    // 레시피의 원재료 수에 맞게 슬롯 생성
+    //    foreach (var ingredientObj in recipe.ingredients)
+    //    {
+    //        Debug.Log("원재료 슬롯 생성 가능?");
+    //        IngredientSlot newSlot = Instantiate(ingredientSlotPrefab, transform);
+
+    //        Debug.Log("원재료 슬롯 위치" + transform);
+
+    //        // 원재료가 CropItemDataInfo 타입
+    //        if (ingredientObj is Ingredient<CropItemDataInfo> cropedIngredient)
+    //        { dlz
+    //            Debug.Log("if문");
+    //            newSlot.SetIngredient(cropedIngredient.item, cropedIngredient.quantity);
+    //        }
+
+    //        // 원재료가 ProcessItemDataInfo 타입
+    //        else if (ingredientObj is Ingredient<ProcessItemDataInfo> processedIngredient)
+    //        {
+    //            Debug.Log("else if문");
+    //            newSlot.SetIngredient(new ProcessItemIItem(processedIngredient.item), processedIngredient.quantity);
+    //        }
+    //        ingredientSlots.Add(newSlot);
+
+    //    }
+    //}
 }
 
